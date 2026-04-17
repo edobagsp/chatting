08d97f7651b8333e87b994af26e974e1302f7df4
edobagsp <edo.marumori.rri@gmail.com>
2026-04-17 15:03:07 +0700

    rev7
    ok

--- vite.config.js
+++ vite.config.js
@@ -5,26 +5,34 @@ export default defineConfig({
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
       manifest: {
         name: 'Retro BBM Chat',
         short_name: 'Chat Apps',
         description: 'Lightweight BBM-themed chat application',
         theme_color: '#0f3c5b',
-        background_color: '#e5e5e5',
+        background_color: '#d0d0d0',
         display: 'standalone',
+        orientation: 'portrait',
         icons: [
           {
             src: 'icon-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: 'icon-512x512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
     })
   ]
 });
+
